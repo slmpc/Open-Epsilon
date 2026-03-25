88e55c45502836745760b38338a00141c9cc01a6
kangleyao <2284736322@qq.com>
2026-03-25 20:56:35 +0800

    feat: add armored elytra flow

--- src/main/java/com/github/lumin/mixins/MixinLivingEntity.java
+++ src/main/java/com/github/lumin/mixins/MixinLivingEntity.java
@@ -3,16 +3,20 @@ package com.github.lumin.mixins;
 import com.github.lumin.events.FallFlyingEvent;
 import com.github.lumin.events.JumpEvent;
 import com.github.lumin.managers.RotationManager;
+import com.github.lumin.modules.impl.player.ElytraFly;
 import com.github.lumin.modules.impl.player.JumpCooldown;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.common.NeoForge;
 import org.joml.Vector2f;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
 public abstract class MixinLivingEntity {
@@ -44,6 +48,13 @@ public abstract class MixinLivingEntity {
     private float onUpdateFallFlyingMovement(LivingEntity instance) {
         FallFlyingEvent event = NeoForge.EVENT_BUS.post(new FallFlyingEvent(instance.getXRot()));
         return event.getPitch();
+    }
+
+    @Inject(method = "travel", at = @At("HEAD"), cancellable = true)
+    private void onTravel(Vec3 travelVector, CallbackInfo ci) {
+        if ((Object) this == Minecraft.getInstance().player && ElytraFly.INSTANCE.handleArmoredTravel()) {
+            ci.cancel();
+        }
     }
 
     @Redirect(method = "aiStep", at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/LivingEntity;noJumpDelay:I", opcode = Opcodes.PUTFIELD, ordinal = 1))
