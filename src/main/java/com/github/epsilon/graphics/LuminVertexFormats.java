5f80a1dfb3ba6ebec5a33f0776a622125dcd8028
Chen Meng <chenmeng466@outlook.com>
2026-04-10 19:20:21 +0800

    修复对 Iris 的兼容性

--- src/main/java/com/github/epsilon/graphics/LuminVertexFormats.java
+++ src/main/java/com/github/epsilon/graphics/LuminVertexFormats.java
@@ -3,15 +3,16 @@ package com.github.epsilon.graphics;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormatElement;
 
+import static com.mojang.blaze3d.vertex.VertexFormatElement.findNextId;
 import static com.mojang.blaze3d.vertex.VertexFormatElement.register;
 
 public class LuminVertexFormats {
 
     public static final VertexFormatElement ROUND_INNER_RECT =
-            register(7, 2, VertexFormatElement.Type.FLOAT, false, 4);
+            register(findNextId(), 2, VertexFormatElement.Type.FLOAT, false, 4);
 
     public static final VertexFormatElement ROUND_RADIUS =
-            register(8, 4, VertexFormatElement.Type.FLOAT, false, 4);
+            register(findNextId(), 4, VertexFormatElement.Type.FLOAT, false, 4);
 
     public static final VertexFormat ROUND_RECT = VertexFormat.builder()
             .add("Position", VertexFormatElement.POSITION)
