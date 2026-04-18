108e3c5d893b84cfb4af15bf813c6c84376ee6b5
Chen Meng <chenmeng466@outlook.com>
2026-04-18 12:26:51 +0800

    优化 control 的宽度

--- neoforge/src/main/java/com/github/epsilon/neoforge/addon/NeoForgePlatformAddon.java
+++ neoforge/src/main/java/com/github/epsilon/neoforge/addon/NeoForgePlatformAddon.java
@@ -1,12 +1,13 @@
 package com.github.epsilon.neoforge.addon;
 
 import com.github.epsilon.Epsilon;
+import com.github.epsilon.addon.EpsilonAddon;
 import com.github.epsilon.neoforge.modules.NeoModuleTest;
 
 /**
  * Built-in NeoForge addon for NeoForge-only features.
  */
-public class NeoForgePlatformAddon extends com.github.epsilon.addon.EpsilonAddon {
+public class NeoForgePlatformAddon extends EpsilonAddon {
 
     public NeoForgePlatformAddon() {
         super("epsilon_neoforge");
